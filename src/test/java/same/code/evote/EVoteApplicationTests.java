7e1625b3a9ec78629f84c9ee71bc9ad27751078d
pascal <samsidinepascalehembasonko4@gmail.com>
2025-01-15 13:22:55 +0000

    premier commit

---
+++ src/test/java/same/code/evote/EVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package same.code.evote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
